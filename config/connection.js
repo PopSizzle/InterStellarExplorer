6efc863ec5dbdb28a92df662a7d0845d5d16f216
Sam Poppe <poppe.samuel@gmail.com>
2020-04-25 11:04:59 -0700

    adding JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 // Require MySQL
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else {
+    connection = mysql.createConnection({
     host:"localhost",
     port: 3306,
     user: "root",
     password: "password",
-    database: "explorer_db" 
+    database: "explorer_db"
 });
 
 // Initiate Connection
