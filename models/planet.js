2a882529faa9dd4330abc4df2b7898af40b9b317
Sam Poppe <poppe.samuel@gmail.com>
2020-04-24 17:58:00 -0700

    add planet model

--- models/planet.js
+++ models/planet.js
@@ -3,6 +3,28 @@ let orm = require("../config/orm.js");
 
 let planet = {
     all: function(cb) {
-        orm.all("")
+        orm.all("planets", function(res) {
+            cb(res);
+        });
+    },
+
+    create: function(cols, vals, cb) {
+        orm.create("planets", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    update: function(objColVals, condition, cb) {
+        orm.update("planets", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+
+    delete: function(condition, cb) {
+        orm.delete("planets", condition, function(res) {
+            cb(res);
+        });
     }
-}
+};
+
+module.exports = planet;
